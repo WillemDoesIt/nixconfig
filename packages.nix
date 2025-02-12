df8385d54f20e88dd8f1e98d9446d022341fda92
Willem Van Zwol <willem@vanzwol.com>
2025-02-12 08:55:59 -0800

    69 current  2025-02-12 08:55:54  24.11.714363.44534bc021b8  6.6.76                          *

--- packages.nix
+++ packages.nix
@@ -1,103 +1,108 @@
 { pkgs, ... }: {
-    environment.systemPackages = with pkgs; [
-        # MINECRAFT STUFF
-        (prismlauncher.override {
-            # Add binary required by some mod
-            additionalPrograms = [ffmpeg];
-
-            # Change Java runtimes available to Prism Launcher
-            jdks = [
-                graalvm-ce
-                zulu8
-                zulu17
-                zulu
-            ];
-        })
-       
-        # typical apps
-        spotify
-        btop
-        alacritty
-        ollama
-        ranger       # terminal file manager
-        nemo         # gui file manager
-        discord
-        vencord      # discord modifier
-        obs-studio
-        obsidian
-        brave        # browser
-        #thunderbird # email client
-        beeper
-        ticktick
-        alejandra
-        steam
-        audacity
-
-        # microsoft
-        p3x-onenote   # kinda useless, just web shit
-        evolution
-        onedrive
+  environment.systemPackages = with pkgs; [
+  
+    # MINECRAFT STUFF
+    (prismlauncher.override {
+      # Add binary required by some mod
+      additionalPrograms = [ffmpeg];
+  
+      # Change Java runtimes available to Prism Launcher
+      jdks = [
+        graalvm-ce
+        zulu8
+        zulu17
+        zulu
+      ];
+    })
         
-        # media viewer
-        vlc
-        mpv # not sure if I wanna keep this
 
-        # utils
-        git
-        man
-        tldr
-        killall
+    # typical apps
+    spotify
+    btop
+    alacritty
+    ollama
+    ranger       # terminal file manager
+    nemo         # gui file manager
+    discord
+    vencord      # discord modifier
+    obs-studio
+    obsidian
+    brave        # browser
 
-        # file editing
-        gimp
-        krita
-        davinci-resolve
-        libreoffice
-        vscode
-        vim
-        neovim
-        #reaper # audio tool
-        
-        # uselesss terminall stuffs
-        neofetch
-        pipes
-        cowsay
-
-        # torrent engines
-        transmission_4-qt
-        #qbittorrent-nox
-        
-        # zip tools
-        zip
-        unzip
-        p7zip-rar
-
-        # screenshot tool
-        hyprshot
-        #flameshot
-        
-        # hyprland stuff 
-        waybar        # menu bar
-        dunst         # notification daemon
-        libnotify
-        rofi-wayland  # program runner
-        swww          # wallpaper daemon
-        playerctl     # media control for shortcuts
-
-        # rust stuff
-        cargo
-
-        # cpp stuff
-        gcc
-        gnumake
-        cmake
-        clang
-        lldb
-
-        # nvchad stuff
-        ripgrep
-        fd
-        lazygit
-        jetbrains-mono # font
-    ];
+    #thunderbird # email client
+    beeper
+    ticktick
+    alejandra
+    steam
+    audacity
+    
+    # microsoft
+    p3x-onenote   # kinda useless, just web shit
+    evolution
+    onedrive
+    
+    # media viewer
+    vlc
+    mpv # not sure if I wanna keep this
+    
+    # utils
+    git
+    man
+    tldr
+    killall
+    
+    # file editing
+    gimp
+    krita
+    davinci-resolve
+    libreoffice
+    vscode
+    vim
+    neovim
+    #reaper # audio tool
+    
+    # uselesss terminall stuffs
+    neofetch
+    pipes
+    cowsay
+    
+    # torrent engines
+    transmission_4-qt
+    #qbittorrent-nox
+    
+    # zip tools
+    zip
+    unzip
+    p7zip-rar
+    
+    # screenshot tool
+    hyprshot
+    #flameshot
+   
+    # hyprland stuff 
+    waybar        # menu bar
+    dunst         # notification daemon
+    libnotify
+    rofi-wayland  # program runner
+    swww          # wallpaper daemon, not sure why I'm not using hyprpaper
+    hyprlock      # lock screen manager
+    playerctl     # media control for shortcuts
+    
+    # rust stuff
+    cargo
+    
+    # cpp stuff
+    gcc
+    gnumake
+    cmake
+    clang
+    lldb
+    
+    # nvchad stuff
+    ripgrep
+    fd
+    lazygit
+    jetbrains-mono # font
+    nerdfonts      # this may not work lmao
+  ];
 }
