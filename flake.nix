acda28adb98523a1098e4ec98e80d79665d7bd54
Willem Van Zwol <willem@vanzwol.com>
2025-03-04 01:55:17 -0800

    142 current  2025-03-04 01:54:58  24.11.20250302.1546c45     6.6.80                          *

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "My sys config in flake form for reproducability";
+
+  inputs = {
+    # NixOS official package source, using the nixos-24.11 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
