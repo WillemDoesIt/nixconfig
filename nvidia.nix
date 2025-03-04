216bddd7923469009dac56e066dac95361eb586b
Willem Van Zwol <willem@vanzwol.com>
2025-03-04 01:22:08 -0800

    nvidia sharded

---
+++ nvidia.nix
@@ -0,0 +1,26 @@
+{ config, ... }: {
+  # NVIDIA STUFF https://nixos.wiki/wiki/Nvidia
+
+  # Enable OpenGL
+  hardware.graphics = {
+    enable = true;
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+
+    # Buggy Experimental Settings
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+
+    # activate menu app
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+}
