0560ba9c45a629d389cf3c823a207e0578bbb5e6
Willem Van Zwol <willem@vanzwol.com>
2025-03-04 10:37:20 -0800

    145 current  2025-03-04 10:37:15  24.11.20250302.1546c45     6.6.80                          *

--- dark-mode.nix
+++ dark-mode.nix
@@ -1,20 +1,20 @@
 { config, pkgs, ... }: {
   environment.variables = {
-    SAL_USE_VCLPLUGIN = "gtk3";
-    GTK_THEME = "Adwaita-dark";
-    QT_QPA_PLATFORMTHEME = "gtk";
+    #SAL_USE_VCLPLUGIN = "gtk3";
+    #GTK_THEME = "Adwaita-dark";
+    #QT_QPA_PLATFORMTHEME = "gtk";
   };
 
   environment.systemPackages = with pkgs; [
-    adwaita-qt
-    gnome.gnome-themes-extra
+    #adwaita-qt
+    #gnome.gnome-themes-extra
     adwaita-qt6
-    breeze-gtk
-    capitaine-cursors
+    #breeze-gtk
+    #capitaine-cursors
     lxappearance
-    gtk3
-    gtk2
-    gtk_engines
-    openjdk  
+    #gtk3
+    #gtk2
+    #gtk_engines
+    #openjdk  
   ];
 }
