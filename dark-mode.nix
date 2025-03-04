6d5f560ff732be91c5774b1060a6a2c26091d511
Willem Van Zwol <willem@vanzwol.com>
2025-03-04 11:02:10 -0800

    151 current  2025-03-04 11:02:05  24.11.20250302.1546c45     6.6.80                          *

--- dark-mode.nix
+++ dark-mode.nix
@@ -7,29 +7,31 @@
     gtk-theme-name=Adwaita-dark;
   '';
 
+  ##
+  # As you can tell a significant portion of this nix file is commented out
+  # this is just because I'm nervous my darkmode will break and I will need these things
+  # they are all stuff chatgpt or other sources have told me were needed for darkmode
+  # but as of right now (2025.03.04) it's working fine as is.
+  ##
+
+  /*
   environment.variables = {
-    #SAL_USE_VCLPLUGIN = "gtk3";
-    #GTK_THEME = "Adwaita-dark";
-    #QT_QPA_PLATFORMTHEME = "gtk";
+    SAL_USE_VCLPLUGIN = "gtk3";
+    GTK_THEME = "Adwaita-dark";
+    QT_QPA_PLATFORMTHEME = "gtk";
     
-    #QT_QPA_PLATFORMTHEME = "qt5ct";
-    #QT_STYLE_OVERRIDE = "kvantum";
+    QT_QPA_PLATFORMTHEME = "qt5ct";
+    QT_STYLE_OVERRIDE = "kvantum";
   };
+  */
 
   environment.systemPackages = with pkgs; [
-    #adwaita-qt
-    gnome.gnome-themes-extra
-    #adwaita-qt6
-    #breeze-gtk
-    #capitaine-cursors
     lxappearance
-    #gtk3
-    #gtk2
-    #gtk_engines
-    #openjdk  
+    gnome.gnome-themes-extra
     
-    #kvantum 
-    #qt5ct
-    #qt6ct
+    /*
+      adwaita-qt adwaita-qt6 breeze-gtk capitaine-cursors 
+      gtk3 gtk2 gtk_engines openjdk kvantum qt5ct qt6ct 
+    */
   ];
 }
