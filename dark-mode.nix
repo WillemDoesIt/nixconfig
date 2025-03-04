43148cab54feeb4121acf4512857d6eb70cddc22
Willem Van Zwol <willem@vanzwol.com>
2025-03-04 10:51:33 -0800

    147 current  2025-03-04 10:51:28  24.11.20250302.1546c45     6.6.80                          *

--- dark-mode.nix
+++ dark-mode.nix
@@ -1,5 +1,6 @@
 { config, pkgs, ... }: {
-  # Enable dark mode for GTK applications
+  # This may be home-manager nix, If so consider this user instructions:
+  # Add this text manually to the `~/.config/gtk-3.0/settings.ini`
   environment.etc."gtk-3.0/settings.ini".text = ''
     [Settings]
     gtk-application-prefer-dark-theme=1
@@ -10,18 +11,25 @@
     #SAL_USE_VCLPLUGIN = "gtk3";
     #GTK_THEME = "Adwaita-dark";
     #QT_QPA_PLATFORMTHEME = "gtk";
+    
+    #QT_QPA_PLATFORMTHEME = "qt5ct";
+    #QT_STYLE_OVERRIDE = "kvantum";
   };
 
   environment.systemPackages = with pkgs; [
-    #adwaita-qt
-    #gnome.gnome-themes-extra
+    adwaita-qt
+    gnome.gnome-themes-extra
     #adwaita-qt6
     #breeze-gtk
     #capitaine-cursors
-    #lxappearance
-    #gtk3
+    lxappearance
+    gtk3
     #gtk2
     #gtk_engines
     #openjdk  
+    
+    #kvantum 
+    #qt5ct
+    #qt6ct
   ];
 }
