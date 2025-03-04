9e78099678fe152ce6d0a3b7046a60cf0dd7beda
Willem Van Zwol <willem@vanzwol.com>
2025-03-04 10:43:58 -0800

    146 current  2025-03-04 10:43:54  24.11.20250302.1546c45     6.6.80                          *

--- dark-mode.nix
+++ dark-mode.nix
@@ -1,4 +1,11 @@
 { config, pkgs, ... }: {
+  # Enable dark mode for GTK applications
+  environment.etc."gtk-3.0/settings.ini".text = ''
+    [Settings]
+    gtk-application-prefer-dark-theme=1
+    gtk-theme-name=Adwaita-dark;
+  '';
+
   environment.variables = {
     #SAL_USE_VCLPLUGIN = "gtk3";
     #GTK_THEME = "Adwaita-dark";
@@ -8,10 +15,10 @@
   environment.systemPackages = with pkgs; [
     #adwaita-qt
     #gnome.gnome-themes-extra
-    adwaita-qt6
+    #adwaita-qt6
     #breeze-gtk
     #capitaine-cursors
-    lxappearance
+    #lxappearance
     #gtk3
     #gtk2
     #gtk_engines
