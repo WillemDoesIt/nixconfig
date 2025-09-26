2ca0169a5314c0d2901d06a4093faa40fc2e7e32
Willem Van Zwol <willem@vanzwol.com>
2025-09-26 09:57:56 -0700

    69 current  2025-09-26 09:55:32  25.05.20250910.8cd5ce8  6.12.46                          *

--- commands/rebuild.sh
+++ commands/rebuild.sh
@@ -69,9 +69,7 @@ find . -type f -name "*.nix" -exec sudo sh -c 'alejandra {} &>/dev/null' \;
 # show staged diff
 # alt: git diff -U0
 # alt: git difftool --no-prompt --extcmd='bat --diff'
-git difftool --no-prompt --extcmd='bat --diff' 2>/dev/null
-
-
+git difftool --no-prompt --extcmd='bat --diff' | rg -v "(/tmp/git-blob-|^─+┬|^─+┼|^─+┴)"
 
 # previous build time
 prev_time=$(<"$time_file" 2>/dev/null || echo 0)
