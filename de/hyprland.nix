2ed0f9ddde633be474edc9477222b6e8a78c0cb7
Willem Van Zwol <willem@vanzwol.com>
2025-10-15 10:40:37 -0700

    109 current  2025-10-15 10:40:32  25.05.20250910.8cd5ce8  6.12.46                          *

--- de/hyprland.nix
+++ de/hyprland.nix
@@ -1,25 +1,16 @@
 {
   config,
   pkgs,
-  lib,
   ...
 }: {
   services.xserver.enable = true;
 
-  services.displayManager.sddm = {
-    enable = true;
-    wayland.enable = true;
-    theme = "chili";
-    settings = {
-      Wayland = {EnableHiDPI = true;};
-      X11 = {ServerArguments = "-nolisten tcp -dpi 200";};
-    };
-  };
+  services.displayManager.ly.enable = true;
 
   programs.hyprland.enable = true;
 
   environment.systemPackages = with pkgs; [
     hyprland
-    sddm-chili-theme
+    ly
   ];
 }
