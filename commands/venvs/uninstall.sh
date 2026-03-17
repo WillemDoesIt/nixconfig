5766e51c6dfd107b58a05134be6b655dc3cb81c1
Willem Van Zwol <willem@vanzwol.com>
2026-03-16 22:10:32 -0700

    158

--- commands/venvs/uninstall.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cd "$(dirname "$0")"
-rm -rf open-webui
-echo "open-webui environment removed."
-
--- packages.nix
+++ packages.nix
@@ -35,16 +35,6 @@
       bash /etc/nixos/commands/nps.sh "$@"
     '')
 
-    # this makes `chat` a command that deploys ollama and it's webui
-    (writeShellScriptBin "chat" ''
-      bash /etc/nixos/commands/venvs/deploy.sh "$@"
-    '')
-
-    # this makes `chatend` a command that stops the deployment of ollama and it's webui
-    (writeShellScriptBin "chatend" ''
-      bash /etc/nixos/commands/venvs/stop.sh "$@"
-    '')
-
     ##
     #  TEMP
     #
@@ -81,7 +71,6 @@
     ##
     kitty
     fish # terminal and shell
-    ollama
     librechat
     yazi # terminal file manager
     #alt: nnn, ranger, lf
