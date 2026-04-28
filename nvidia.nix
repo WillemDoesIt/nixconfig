f05a1656b332a38b60e1b90159a415c2554799cf
Willem Van Zwol <willem@vanzwol.com>
2026-04-28 10:16:03 -0700

    177

--- nvidia.nix
+++ nvidia.nix
@@ -7,25 +7,27 @@
   };
 
   # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
+  # services.xserver.videoDrivers = ["nvidia"];
 
   # Intel first, NVIDIA secondary (offload only)
-  environment.sessionVariables = {
-    WLR_DRM_DEVICES = "/dev/dri/card1:/dev/dri/card2";
-  };
+  # environment.sessionVariables = {
+  #   WLR_DRM_DEVICES = "/dev/dri/card1:/dev/dri/card2";
+  # };
 
   hardware.nvidia = {
     modesetting.enable = true;
-
-    # Buggy Experimental Settings
-    powerManagement.enable = false;
-    powerManagement.finegrained = false;
     open = false;
-
-    # activate menu app
     nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    powerManagement.enable = true;
+  };
+
+  hardware.nvidia.prime = {
+    offload.enable = true;
+    offload.enableOffloadCmd = true;
+
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0";
   };
 }
