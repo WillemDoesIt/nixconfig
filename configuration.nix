6480b983a67c1b0d18bb5b8a5aee4652b9848a46
Willem Van Zwol <willem@vanzwol.com>
2026-07-16 11:05:36 -0700

    198

--- configuration.nix
+++ configuration.nix
@@ -71,28 +71,10 @@
     syncthing = {
       enable = true;
       openDefaultPorts = true;
-      #dataDir = "/home/willemvz";
-      #settings = {
-      #  gui = {
-      #    user = "myuser";
-      #    password = "mypassword";
-      #  };
-      #  devices = {
-      #    "device1" = { id = "DEVICE-ID-GOES-HERE"; };
-      #    "device2" = { id = "DEVICE-ID-GOES-HERE"; };
-      #  };
-      #  folders = {
-      #    "Documents" = {
-      #      path = "/home/myusername/Documents";
-      #      devices = [ "device1" "device2" ];
-      #    };
-      #    "Example" = {
-      #      path = "/home/myusername/Example";
-      #      devices = [ "device1" ];
-      #      ignorePerms = false; # Enable file permission syncing
-      #    };
-      #  };
-      #};
+      user = "willemvz";
+      group = "users"; # or your primary group
+      dataDir = "/home/willemvz/.local/share/syncthing";
+      configDir = "/home/willemvz/.config/syncthing";
     };
 
     # i2pd = {
